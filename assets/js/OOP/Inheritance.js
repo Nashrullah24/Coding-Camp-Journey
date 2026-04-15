3e9d34dfb0c513441de36f350d466b19687777b9
Nashullah24 <nabiliman247@gmail.com>
2026-04-15 21:44:35 +0700

    soal 2 -- inheritance

--- assets/js/OOP/Inheritance.js
+++ assets/js/OOP/Inheritance.js
@@ -40,3 +40,43 @@ motor.jalan();
 motor.wheelie();
 mobil.berhenti();
 mobil.klakson();
+
+// soal 2
+class Pegawai {
+  constructor(nama, gaji) {
+    this.nama = nama;
+    this.gaji = gaji;
+  }
+
+  info() {
+    return `Nama: ${this.nama} | Gaji: ${this.gaji}`;
+  }
+}
+
+class Manager extends Pegawai {
+  constructor(nama, gaji, divisi) {
+    super(nama, gaji);
+    this.divisi = divisi;
+  }
+  info() {
+    console.log(`${super.info()} | Divisi: ${this.divisi}`);
+  }
+}
+
+class Intern extends Pegawai {
+  constructor(nama, gaji, durasi) {
+    super(nama, gaji);
+    this.durasi = durasi;
+  }
+
+  info() {
+    console.log(`${super.info()} | Durasi Magang: ${this.durasi}`);
+  }
+}
+
+const manager = new Manager("yadi", 5000000, "IT");
+const intern = new Intern("Sari", 2000000, "3 Bulan");
+
+manager.info();
+intern.info();
+pegawai.info();
